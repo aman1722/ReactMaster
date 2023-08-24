372d62c2d6f68ccd7e9f5a8be9d892d82905010f
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-08-24 10:32:47 +0530

    challange-1

---
+++ Challange-1/netflixlist/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+ReactDOM.render(
+
+  <>
+  <h1>Aman Netflix Pick</h1>
+  <p>Here is the list of my fav netflix series</p>
+  <ol>
+    <li>Series-1</li>
+    <li>Series-2</li>
+    <li>Series-3</li>
+    <li>Series-4</li>
+    <li>Series-5</li>
+  </ol>
+  </>,document.getElementById("root")
+
+);
